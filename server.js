cc6634197156b7c6df86c2aa588860553ff2cc30
Angad Singh <mail@angad.dev>
2019-03-31 19:47:36 +0530

    Done with everything except UI

--- server.js
+++ server.js
@@ -1,32 +1,128 @@
-const http = require('http')
+const Koa = require('koa')
+const bodyParser = require('koa-bodyparser')
+const views = require('koa-views')
+const Router = require('koa-router')
+const fs = require('fs')
+const { promisify } = require('util')
+const storage = require('./storage')
+const app = new Koa()
+const router = new Router()
 
-http
-  .createServer({}, (req, res) => {
-    // console.log(req.url)
+app.use(bodyParser())
+app.use(views('html', { extension: 'html' }))
 
-    if (req.url.match(/^\/gh\/\w+/gi)) {
-      const repo = req.url.split('/')[2]
-      res.writeHead(301, {
-        Location: `https://github.com/dotangad/${repo}`
-      })
-      res.end()
-    } else if (req.url.match(/^\/gp\/\w*/gi)) {
-      const repo = req.url.split('/')[2]
-      res.writeHead(301, {
-        Location: `https://dotangad.github.io/${repo}`
-      })
-      res.end()
-    } else if (req.url === '/gh') {
-      res.writeHead(301, {
-        Location: 'https://github.com/dotangad'
-      })
-      res.end()
-    } else {
-      res.writeHead(200, {
-        'Content-Type': 'text/plain'
-      })
-      res.write(req.url)
-      res.end()
-    }
-  })
-  .listen(3006)
+app.use(async (ctx, next) => {
+  console.log(ctx.request.url)
+  await next()
+})
+
+router.get('/gh', async ctx => ctx.redirect('https://github.com/dotangad'))
+
+router.get('/gh/:repo', async ctx =>
+  ctx.redirect(
+    `https://github.com/dotangad/${ctx.req.url.split('/')[2].split('?')[0]}`
+  )
+)
+
+router.get('/gp/:repo', async ctx =>
+  ctx.redirect(
+    `https://dotangad.github.com/${ctx.req.url.split('/')[2].split('?')[0]}`
+  )
+)
+
+router.get('/all', async ctx => {
+  // Check password
+  const { password } = ctx.request.query
+  const correctPassword = (await promisify(fs.readFile)(
+    './password',
+    'utf-8'
+  )).replace('\n', '')
+
+  if (!password === correctPassword) {
+    ctx.status = 401
+    ctx.body = 'Incorrect password'
+    return
+  }
+
+  ctx.body = await storage.readShortlinks('./shortlinks.json')
+})
+
+router.post('/create', async ctx => {
+  // Check password
+  const { password } = ctx.request.query
+  const correctPassword = (await promisify(fs.readFile)(
+    './password',
+    'utf-8'
+  )).replace('\n', '')
+
+  if (password !== correctPassword) {
+    ctx.status = 401
+    ctx.body = 'Incorrect password'
+    return
+  }
+
+  const { shortlink, url } = ctx.request.body
+  if (
+    !shortlink.match(/^[a-z]+$/i) ||
+    !url.match(
+      /^(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})$/gi
+    )
+  ) {
+    ctx.status = 400
+    ctx.body = 'Bad params'
+    return
+  }
+
+  // Create shortlink
+  if (await storage.createShortlink('./shortlinks.json', shortlink, url)) {
+    ctx.status = 200
+    ctx.body = 'Created'
+    return
+  } else {
+    ctx.status = 400
+    ctx.body = 'Exists'
+    return
+  }
+})
+
+router.delete('/:shortlink', async ctx => {
+  // Check password
+  const { password } = ctx.request.query
+  const correctPassword = (await promisify(fs.readFile)(
+    './password',
+    'utf-8'
+  )).replace('\n', '')
+
+  if (password !== correctPassword) {
+    ctx.status = 401
+    ctx.body = 'Incorrect password'
+    return
+  }
+
+  if (
+    await storage.deleteShortlink(
+      './shortlinks.json',
+      ctx.request.url.split('/')[1].split('?')[0]
+    )
+  ) {
+    ctx.status = 200
+    ctx.body = 'Deleted'
+    return
+  }
+
+  ctx.status = 400
+  ctx.body = "Doesn't exist"
+})
+
+router.get('/:shortlink', async ctx => {
+  // Find url
+  const url = await storage.findURL(
+    './shortlinks.json',
+    ctx.req.url.split('/')[1].split('?')[0]
+  )
+  if (url) ctx.redirect(url)
+  else ctx.redirect('https://angad.dev')
+})
+
+app.use(router.routes(), router.allowedMethods())
+app.listen(3006)
--- storage.js
+++ storage.js
@@ -1,16 +1,42 @@
 const fs = require('fs')
+const { promisify } = require('util')
+const readFile = promisify(fs.readFile)
+const writeFile = promisify(fs.writeFile)
 
 async function readShortlinks(fileName) {
-  return JSON.parse(await fs.readFile(filename))
+  return JSON.parse(await readFile(fileName))
 }
 
 async function createShortlink(fileName, shortlink, url) {
-  const shortlinks = await readShortlinks()
+  const shortlinks = await readShortlinks(fileName)
+
+  // Check if shortlink already exists
+  if (shortlinks.find(s => s.shortlink === shortlink)) return false
+
   shortlinks.push({ shortlink, url })
-  await fs.writeFile(fileName, JSON.stringify(shortlinks))
+  await writeFile(fileName, JSON.stringify(shortlinks))
+  return true
+}
+
+async function findURL(fileName, shortlink) {
+  const shortlinks = await readShortlinks(fileName)
+  const found = shortlinks.find(s => s.shortlink === shortlink)
+  return found ? found.url : false
+}
+
+async function deleteShortlink(fileName, shortlink) {
+  const shortlinks = await readShortlinks(fileName)
+  const found = shortlinks.findIndex(s => s.shortlink === shortlink)
+  if (found !== -1) shortlinks.splice(found, 1)
+  else return false
+
+  await writeFile(fileName, JSON.stringify(shortlinks))
+  return true
 }
 
 module.exports = {
   readShortlinks,
-  createShortlink
+  createShortlink,
+  findURL,
+  deleteShortlink
 }
